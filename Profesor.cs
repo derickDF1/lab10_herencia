a04d46e0928ac01e8b25797e363baf5bb911c3fc
derickcux2023 <derickcux2023@LAB4-21>
2024-05-02 09:43:31 -0600

    clase2-05-2024.

--- Profesor.cs
+++ Profesor.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace lab10_herencia
 {
     public class Profesor : Persona
     {
+        [Required(ErrorMessage = "El titulo es requerido")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "El Colegiado es requerido es requerido")]
+        [StringLength(6, ErrorMessage = "Deben ser menos de 6 digitos")]
         public int NumColegiado { get; set; }
 
         public Profesor()
