90ad03a06114bf8adaa2321aa071f43afd22805c
derickcux2023 <derickcux2023@LAB4-20>
2024-05-06 09:40:59 -0600

    06-05-2024, corregir registros duplicados.

--- Profesor.cs
+++ Profesor.cs
@@ -1,25 +1,25 @@
 ﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
 
 namespace lab10_herencia
 {
     public class Profesor : Persona
     {
+
         [Required(ErrorMessage = "El titulo es requerido")]
         public string Titulo { get; set; }
         [Required(ErrorMessage = "El Colegiado es requerido es requerido")]
         [StringLength(6, ErrorMessage = "Deben ser menos de 6 digitos")]
-        public int NumColegiado { get; set; }
-        public string NumeroColegiado { get; set; }
+        public string NumColegiado { get; set; }
 
         public Profesor()
         {
             Titulo = string.Empty;
-            NumeroColegiado = string.Empty;
+            NumColegiado = string.Empty;
         }
     }
 }
