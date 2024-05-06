e9a4ea2d4af6d87a98610034abb34b109bd423af
derickcux2023 <derickcux2023@LAB4-20>
2024-05-06 09:15:39 -0600

    06/05/2024

--- Profesor.cs
+++ Profesor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
@@ -13,11 +14,12 @@ namespace lab10_herencia
         [Required(ErrorMessage = "El Colegiado es requerido es requerido")]
         [StringLength(6, ErrorMessage = "Deben ser menos de 6 digitos")]
         public int NumColegiado { get; set; }
+        public string NumeroColegiado { get; set; }
 
         public Profesor()
         {
             Titulo = string.Empty;
-            NumColegiado = 0;
+            NumeroColegiado = string.Empty;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
+using Blazored.LocalStorage;
 namespace lab10_herencia
 {
     public class Program
